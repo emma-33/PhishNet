292e454d9146b616daccb05e6d8006e0b7df77fd
Diogo <diogoanonimos@gmail.com>
2026-01-09 00:11:46 +0100

    [Fix] Mistake on dotenv where it wouldn't get loaded

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,10 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+env_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path=env_path)
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
